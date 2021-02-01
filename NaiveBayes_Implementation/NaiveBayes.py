73a36e0b9be8ab889c0fc542ff4e911d065426d9
Jan Brebeck <Jan.Brebeck98@gmail.com>
2021-02-01 13:17:22 +0100

    Refactorings during code review

--- NaiveBayes_Implementation/NaiveBayes.py
+++ NaiveBayes_Implementation/NaiveBayes.py
@@ -7,31 +7,22 @@ from sklearn.feature_extraction.text import TfidfTransformer
 from sklearn.naive_bayes import MultinomialNB
 import statistics 
 
-def dicts_without_duplication(dict1,dict2):
-    for word in list(dict1.keys()):
-        if word in dict2:
-            dict2.pop(word)
-            dict1.pop(word)
-
-    for word in list(dict2.keys()):
-        if word in dict1:
-            dict2.pop(word)
-            dict1.pop(word)
-    return dict1, dict2
-
 def binary_class_infos(data,label_column_name,label_1,label_2):
-    grouped_data= data.groupby(label_column_name)
+    # group data by label
+    grouped_data = data.groupby(label_column_name)
 
+    # calculate lengths
     len_all = len(train_data)
     len_positive = len(grouped_data.get_group(label_1)) 
     len_negative = len(grouped_data.get_group(label_2))
 
+    # calculate class probs
     prob_positive = len_positive/len_all
     prob_negative = len_negative/len_all
     
     return len_all, len_positive, len_negative, prob_positive, prob_negative
 
-def calc_document_probabilities(words,positive_corpus,negative_corpus,len_positive,len_negative,threshold,smoothing,without):
+def calc_document_probabilities(document,positive_corpus,negative_corpus,len_positive,len_negative,threshold,smoothing,without):
     positive_weights=[]
     negative_weights=[]
     
@@ -44,7 +35,7 @@ def calc_document_probabilities(words,positive_corpus,negative_corpus,len_positi
         beta=0
 
     # Go trough all words of the given document
-    for word in words:
+    for word in document:
         # reset prob
         prob=0
         if word in positive_corpus:
@@ -73,23 +64,26 @@ def calc_document_probabilities(words,positive_corpus,negative_corpus,len_positi
 def train_naive_bayes(data):
     positive_corpus={}
     negative_corpus={}
+    # count the wordfrequency for both corpus
     for index, row in data.iterrows():
         words = row["News"]
+        # check for class
         if row["Label"] == 1:
+            # iterrate over document
             for word in words:
+                # check if word is already in corpus, if so add 1 if not add the word with value 1
                 if word in positive_corpus:
                     positive_corpus[word]+=1
                 else:
                     positive_corpus[word]=1
         else:
+            # iterrate over document
             for word in words:
+                # check if word is already in corpus, if so add 1 if not add the word with value 1
                 if word in negative_corpus:
                     negative_corpus[word]+=1
                 else:
                     negative_corpus[word]=1
-
-    # print(f"Mean count of positive_corpus words in training {statistics.mean(positive_corpus.values())}")
-    # print(f"Mean count of negative_corpus words in training {statistics.mean(negative_corpus.values())}")
 
     return positive_corpus, negative_corpus
 
@@ -99,7 +93,7 @@ def calculate_accuracy(predicted_labels, labels):
     fn=0
     tn=0
 
-    # Calculated accuracy based on predicted_labels
+    # Calculated tp, tn, fp and fn based on predicted_labels and correct labels
     for real, pred in zip(labels, predicted_labels):
         if real == 1:
             if pred == 1:
@@ -111,55 +105,62 @@ def calculate_accuracy(predicted_labels, labels):
                 fp +=1
             elif pred == -1:
                 tn +=1
+    # Calculated accuracy based on tp, tn, fp and fn 
     return (tp+tn)/(fp+tp+fn+tn), tp, tn, fp, fn
 
 def eval_naive_bayes(data,positive_corpus,negative_corpus, smoothing, without):
-    counter =0
+    # get data informations
     len_all, len_positive, len_negative, prob_positive, prob_negative = binary_class_infos(train_data,"Label",1,-1)
     
     threshold_values=[]
     accuracy_values=[]
     prediction_results=[]
+    # use different thresholds
     for threshold in np.arange(0.0001,0.1,0.0001):
+        # if we use words that are not in the train corpus, we need to set the threshold to 0
         if not without:
             threshold = 0
         labels=[]
         predicted_labels=[]
+        # save used threshold
         threshold_values.append(threshold)
+        # iterrate over test_data
         for index, row in test_data.iterrows():
             words = row["News"]
             label = row["Label"]
             # Calculate probabilities for all words and both classes
-            positive_result,negative_result = calc_document_probabilities(words,positive_corpus,negative_corpus,len_positive,len_negative,threshold,smoothing,without)
+            positive_document_prob,negative_document_prob = calc_document_probabilities(words,positive_corpus,negative_corpus,len_positive,len_negative,threshold,smoothing,without)
             # Sumup the results
-            results={1:positive_result*prob_positive,-1:negative_result*prob_negative}
+            results={1:positive_document_prob*prob_positive,-1:negative_document_prob*prob_negative}
             # Get predicted label by highest probability and add actual label for comparison
             predicted_labels.append(max(results, key=results.get))
             labels.append(label)
+        # calculate accuracy and save results
+
         accuracy, tp, tn, fp, fn = calculate_accuracy(predicted_labels,labels)
         accuracy_values.append(accuracy)
         prediction_results.append({"tp":tp,"tn":tn,"fn":fn,"fp":fp})
+        
+        # if we don't use the threshold, we dont need to itterate over different thresholds.
         if not without:
             break
-        # elif counter == 2:
-        #     break
-        # counter+=1
-        
         
     return threshold_values, accuracy_values, prediction_results
 
 def generate_summary(thresholds, accuracy, results, test_data_len, add_to_filename=""):
-    best_index = accuracy.index(max(accuracy))
+    # get index of best accuracy
+    best_accuracy = max(accuracy)
+    best_index = accuracy.index(best_accuracy)
+    # print best threshold
     print(thresholds[best_index])
-    generate_result_document(test_data_len,results[best_index]["tp"],results[best_index]["tn"],results[best_index]["fp"],results[best_index]["fn"],add_to_filename)
+    # generate output document
+    generate_result_document(test_data_len,best_accuracy,results[best_index]["tp"],results[best_index]["tn"],results[best_index]["fp"],results[best_index]["fn"],add_to_filename)
     
-def generate_result_document(test_data_len,tp,tn,fp,fn,add_to_filename=""):
-    data_amount = str(test_data_len)
-    accuracy = str((tp+tn)/(tp+tn+fp+fn))
-
+def generate_result_document(test_data_len,accuracy,tp,tn,fp,fn,add_to_filename=""):
+    # create outputcontent
     output = f"""
     |--------------------------------------------------------------
-    | the overall amount of data ist: {data_amount}
+    | the overall amount of data ist: {test_data_len}
     |--------------------------------------------------------------
     | the true positive_corpus rate is:      {tp}
     | the true negative_corpus rate is:      {tn}
@@ -169,17 +170,12 @@ def generate_result_document(test_data_len,tp,tn,fp,fn,add_to_filename=""):
     | the accuracy is:                {accuracy}
     |--------------------------------------------------------------
     """
-
     print(output)
+    # save to file
     filename =  f"./output/output_{add_to_filename}_{time.strftime('%Y%m%d-%H%M%S')}.txt"
     outputfile = open(filename,"w")
     outputfile.write(output) 
     outputfile.close()
-
-def plot_accuracy(x,y):
-    plt.plot(x,y)
-    plt.ylabel('accuracy.')
-    plt.show()
 
 ################################################################################################
 
@@ -198,11 +194,11 @@ thresholds3, accuracy3, results3 = eval_naive_bayes(test_data,positive_corpus, n
 thresholds4, accuracy4, results4 = eval_naive_bayes(test_data,positive_corpus, negative_corpus, smoothing=True, without=True)
 
 # show evaluationresults and generate outputfiles
-print("Ohne Smoothing und berücksichtigung nicht vorhandener Wörter im Trainingskorpus")
+print("Ohne Smoothing und mit berücksichtigung nicht vorhandener Wörter im Trainingskorpus")
 generate_summary(thresholds1, accuracy1, results1,test_data_len,"WithoutSmoothAndDropping")
-print("Mit Smoothing und berücksichtigung nicht vorhandener Wörter im Trainingskorpus")
+print("Mit Smoothing und mit berücksichtigung nicht vorhandener Wörter im Trainingskorpus")
 generate_summary(thresholds2, accuracy2, results2,test_data_len,"WithSmoothAndWithoutDropping")
-print("Ohne Smoothing und nicht vorhandene Wörter im Trainingskorpus")
+print("Ohne Smoothing und ohne nicht vorhandene Wörter im Trainingskorpus")
 generate_summary(thresholds3, accuracy3, results3,test_data_len,"WithoutSmoothAndWithDropping")
 print("Mit Smoothing und ohne nicht vorhandene Wörter im Trainingskorpus")
 generate_summary(thresholds4, accuracy4, results4,test_data_len,"WithSmoothAndDropping")
